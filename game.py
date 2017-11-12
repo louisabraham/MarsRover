ec00f9db73c4e2f2223ce1796d9a1bcce47ed728
louisabraham <louis.abraham@yahoo.fr>
2017-11-12 01:38:56 +0100

    retrieves the canvas image directly, thanks @intermet
    offers chrome and chromium with headless option

--- game.py
+++ game.py
@@ -1,50 +1,79 @@
 from time import sleep
-from os import system
 import io
+import base64
+from hashlib import md5
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from scipy import misc
-import matplotlib.pyplot as plt
-# from selenium.webdriver.common.keys import Keys
 
-from image import wheels, road, demo as image_demo
 
 URL = "https://d29zfk7accxxr5.cloudfront.net/games/game-142/data/index.html"
-RESOLUTION = 512
 
 
 class Game():
 
-    def __init__(self, res=RESOLUTION):
-        # TODO: use chromedriver with or without headless
-        # (https://intoli.com/blog/running-selenium-with-headless-chrome/)
-        
-        # --disk-cache=true allows to keep a cache
-        driver = webdriver.PhantomJS(service_args=['--disk-cache=true'])
-        driver.set_window_size(res, res)
-        driver.get(URL)
-        body = driver.switch_to_active_element()
-        sleep(.2)
-        self.driver = driver
-        self.body = body
+    def __init__(self, driver):
+        driver = driver.casefold()
 
-    def start(self):
-        self.body.send_keys('r')
+        if driver == 'phantomjs':
+            # --disk-cache=true allows to keep a cache
+            self.driver = webdriver.PhantomJS(
+                service_args=['--disk-cache=true'])
+                
+        elif driver in ['chrome', 'chrome-headless', 'chromium', 'chromium-headless']:
+            chrome_options = ChromeOptions()
+            chrome_options.add_argument("--disable-gpu")  # most important line
+            chrome_options.add_argument("--disable-extensions")
+            if len(driver.split('-')) == 2:
+                chrome_options.add_argument("--headless")
+            if driver.split('-')[0] == 'chromium':
+                chrome_options.binary_location = '/Applications/Chromium.app/Contents/MacOS/Chromium'
+            self.driver = webdriver.Chrome(chrome_options=chrome_options)
+
+        elif driver == 'firefox':
+            # not working, disable gpu?
+            self.driver = webdriver.Firefox()
+
+        self.driver.get(URL)
+
+        # hash of starting screen
+        magic = '2979145a2e1a7685c995a8e097276cd1'
+        while self.screen_hash() != magic:
+            sleep(.1)
+            # print(self.screen_hash())
 
     def screen(self, file=None):
         """
         Does a screenshot
 
-        By default, returns the screen as png, the wheels and the road.
-        screen(True) displays the processed screen
-        screen(file) saves the screen
+        returns the screen
         """
-        f = misc.imread(io.BytesIO(self.driver.get_screenshot_as_png()))
-        if file is None:
-            return f, wheels(f), road(f)
-        elif file is True:
-            image_demo(f)
-            plt.savefig('/tmp/screenMars.png')
-            system('open /tmp/screenMars.png')
-        else:
-            self.driver.save_screenshot(file)
+        data = self.driver.execute_script(
+            "return document.getElementById('canvas').toDataURL()")[22:]
+        data = base64.b64decode(data)
+        img = misc.imread(io.BytesIO(data))
+        misc.imsave('/tmp/mars.png', img)
+        return img
+
+    def screen_hash(self):
+        """
+        return hash of screen
+        """
+        return md5(bytes(self.screen())).digest().hex()
+
+    def start(self):
+        self.driver.execute_script('gamee.onRestart()')
+
+    def pause(self):
+        self.driver.execute_script('gamee.onPause()')
+
+    def resume(self):
+        self.driver.execute_script('gamee.onResume()')
+
+    def score(self):
+        'can do more accurate'
+        return self.driver.execute_script('return gamee.score')
+
+    def over(self):
+        return self.screen_hash() == '3f0371f975116bb1165834d64c4c67b0'
