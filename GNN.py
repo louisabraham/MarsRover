890d4d1a94074e7d90d2171e5f9d3616defad3f9
louisabraham <louis.abraham@yahoo.fr>
2017-11-13 23:02:46 +0100

    correct __repr__ of SimpleNN

--- GNN.py
+++ GNN.py
@@ -17,17 +17,19 @@ def ReLU(x):
 
 class SimpleNN(namedtuple("SimpleNN", 'W, C')):
 
-    def __new__(cls, *args, activation='ReLU'):
+    def __new__(cls, *args, **kwargs):
         if len(args) == 1:
             layers, = args
             W = [rnd.standard_normal((i, j))
                  for i, j in zip(layers[:-1], layers[1:])]
             C = rnd.standard_normal(len(layers) - 1)
         else:
+            if not args:
+                args = kwargs['W'], kwargs['C']
             W, C = args
         return super().__new__(cls, W, C)
 
-    def __init__(self, *args, activation):
+    def __init__(self, *args, activation='ReLU', **kwargs):
         self.activation = eval(activation)
 
     def evaluate(self, inp):
@@ -61,18 +63,23 @@ class SimpleNN(namedtuple("SimpleNN", 'W, C')):
         C = SimpleNN.aux_mutate(a.C, MUTATION_PARAM)
         return a.__class__(W, C, activation=a.activation.__name__)
 
-
+    def __repr__(self):
+        return '%s, activation=%r)' % (super().__repr__()[:-1], self.activation.__name__)
+        
+        
 class controllerNN(SimpleNN):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, activation='ReLU', **kwargs):
         if len(args) == 1:
             layers, = args
             assert layers[-1] == 6
         else:
+            if not args:
+                args = kwargs['W'], kwargs['C']
             W, C = args
             assert W[-1].shape[1] == 6
         super().__init__(*args, **kwargs)
-
+    
     def evaluate(self, inp):
         ans = super().evaluate(inp)
         return np.argmax(ans[:3]) - 1, np.argmax(ans[3:]) - 1
