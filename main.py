5f0b79cf80d8b2c86f0b9ef7acbb9f11840b03c7
louisabraham <louis.abraham@yahoo.fr>
2017-11-13 18:21:56 +0100

    add evolution strategy (1+4+1)

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from time import sleep
+from itertools import product
+
+import numpy as np
 
 from game import Game
 from GNN import controllerNN
 from image import road, wheels
-
 from sensors import slope, proximity_sensors, default_sensor_directions
 
 
@@ -20,10 +22,10 @@ def inputs(game):
 def execute(game, controller, timestep=0, async=True, timeout=10):
     """
     Tests the controller against a full run
-    
+
     timestep can be 0 because there is a delay
-    from the inputs collection (about 320 ms)
-    
+    from the inputs collection (about 300 ms)
+
     timeout avoids looping because of a null controller
     """
     game.restart()
@@ -34,18 +36,38 @@ def execute(game, controller, timestep=0, async=True, timeout=10):
             return 0
         if not async:
             game.pause()
-        keys = controller.evaluate(inputs(game))
+        try:
+            inp = inputs(game)
+        except Exception as e:
+            # print('error', e)
+            return game.score()
+        keys = controller.evaluate(inp)
         game.control(*keys)
         # print('\r' + str(keys))
         if not async:
             game.resume()
         sleep(timestep)
+        # print('\r' + str(game.score()), end='')
     return game.score()
 
 if __name__ == '__main__':
     game = Game('chrome')
     game.mute()
-    d = time();inputs();print(time()-d)
-    controller = controllerNN([16, 32, 32, 6])
-    execute(game, controller)
-    
+
+    def fit(controller):
+        return sum(execute(game, controller) for _ in range(2)) / 2
+
+    def new():
+        return controllerNN([16, 8, 8, 6], activation='sigmoid')
+
+    best = new()
+    while not fit(best):
+        best = new()
+
+    for gen in range(50):
+        pop = [best] + [best.mutate() for _ in range(4)] + [new()]
+        scores = list(map(fit, pop))
+        print('gen %s :' % gen, scores)
+        best = pop[np.argsort(scores)[-1]]
+
+    print(scores)
