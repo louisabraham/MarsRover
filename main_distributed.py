7d3e81838447dd895aae2116ee985503519a0ea6
louisabraham <louis.abraham@yahoo.fr>
2017-11-15 00:03:48 +0100

    main_distributed

--- main_distributed.py
+++ main_distributed.py
@@ -1,24 +1,31 @@
-
 import numpy as np
+
 from GNN import controllerNN
 
-from executor import Executor
+from remote_executor import PoolManager
 
 
 if __name__ == '__main__':
 
-    fit = Executor('chrome').fit
+    N = 6
+
+    pm = PoolManager(N)
+    fit = pm.pool[0].fit
 
     def new():
         return controllerNN([17, 8, 8, 6], activation='sigmoid')
 
-    best = new()
-    while not fit(best):
-        best = new()
+    pop = [new() for _ in range(N)]
+    scores = pm.map_fit(pop)
+    while not any(scores):
+        pop = [new() for _ in range(N)]
+        scores = pm.map_fit(pop)
+
+    best = pop[np.argsort(scores)[-1]]
 
     for gen in range(50):
         pop = [best] + [best.mutate() for _ in range(4)] + [new()]
-        scores = list(map(fit, pop))
+        scores = pm.map_fit(pop)
         print('gen %s :' % gen, scores)
         best = pop[np.argsort(scores)[-1]]
 
